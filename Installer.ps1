03b66861a6de6901714103b34d6f6ae5599c4ed6
phrasz <dev@null.com>
2013-07-22 02:06:18 -0400

    Lib FLAC and DUMB; found links for most remaining libraries

--- Installer.ps1
+++ Installer.ps1
@@ -174,8 +174,8 @@ if($result -eq 0){
 	Write-Host
 	$NCurrent += 1
 
-$result = $Host.UI.PromptForChoice("[$NCurrent/$N] Would you like to install verified dependency packages??<DEBUGGING>","",$choices,0)
-if($result -eq 0){
+$result2 = $Host.UI.PromptForChoice("[$NCurrent/$N] Would you like to install verified dependency packages??<DEBUGGING>","",$choices,0)
+if($result2 -eq 0){
 	####################################
 	#	0) ENV VARs 
 		
@@ -238,9 +238,6 @@ if($result -eq 0){
 	Write-Host "[$NCurrent/$N] `tDownloading: $OggDL ..."
 	
 	(New-Object System.Net.WebClient).DownloadFile($OggDL,"$env:HOMEDRIVE$env:HOMEPATH\Ogg_src.zip")
-
-	
-
 	
 	Move-Item $env:HOMEDRIVE$env:HOMEPATH\Ogg_src.zip .
 	Write-Host "[$NCurrent/$N] `tExtracting the source..."
@@ -250,7 +247,7 @@ if($result -eq 0){
 	Write-Host "[$NCurrent/$N] `tCreating MSYS build shell script..."
 	$t1="#!/bin/sh"
 	Write-Output $t1 | Out-File -encoding ASCII .\oggcmd.sh -width 200
-	$t1="cd ~/Allegro5.1/A5_Deps/Ogg/libogg-1.3./"
+	$t1="cd ~/Allegro5.1/A5_Deps/Ogg/libogg-1.3.1/"
 	Write-Output $t1 | Out-File -encoding ASCII .\oggcmd.sh -width 200 -append
 	#$t1="./configure --prefix=`"``echo ~/``Allegro5.1/A5_Deps`""
 	$t1="./configure --prefix=`"/c/mingw`""
@@ -273,7 +270,7 @@ if($result -eq 0){
 	#$file.Attributes = 'Hidden'
 	Move-Item .\oggcmd.sh "$DestSh\" -force
 	#copy $env:HOMEDRIVE$env:HOMEPATH\Allegro5.1\MinGW\msys\1.0\msys.bat .
-	& C:\MinGW\msys\1.0\msys --login
+	& C:\MinGW\msys\1.0\msys --login #should only need to be called by last ps scripted
 
 	
 	####################################
@@ -333,26 +330,128 @@ if($result -eq 0){
 	& C:\MinGW\msys\1.0\msys --login
 	
 	####################################
-}
-if($result -eq 1) { 
-	Write-Host "[$NCurrent/$N] Skipping Verified Dependency Packages Installation" 
-}	
+
 	Write-Host
 	$NCurrent += 1
 
 	####################################
 	#    4) FLAC
-#http://downloads.xiph.org/releases/flac/flac-1.3.0.tar.xz
+	$flac = "flac"
+	Write-Host "[$NCurrent/$N] Creating flac Libary"
+	Write-Host "[$NCurrent/$N] `tMaking Directory: .\$flac"
+	$Destflac = ".\$flac"
+	# if folder does not exist...
+	if (!(Test-Path $Destflac)){
+		# create it
+		[void](new-item $flac -itemType directory)
+	}
+	cd $flac | out-null
+	
+	$flacDL="http://downloads.xiph.org/releases/flac/flac-1.3.0.tar.xz"
+	Write-Host "[$NCurrent/$N] `tDownloading: $flacDL ..."
+	
+	(New-Object System.Net.WebClient).DownloadFile($flacDL,"$env:HOMEDRIVE$env:HOMEPATH\flac_src.tar.xz")
+	
+	Move-Item $env:HOMEDRIVE$env:HOMEPATH\flac_src.tar.xz .
+	Write-Host "[$NCurrent/$N] `tExtracting the source..."
+	& "7z" x flac_src.tar.xz
+	& "7z" x flac_src.tar
+	Remove-Item flac_src.tar -recurse
+	Remove-Item flac_src.tar.xz -recurse
 
-
-
+	#cd libflac-1.3.1
+	Write-Host "[$NCurrent/$N] `tCreating MSYS build shell script..."
+	$t1="#!/bin/sh"
+	Write-Output $t1 | Out-File -encoding ASCII .\flaccmd.sh -width 200
+	$t1="cd ~/Allegro5.1/A5_Deps/flac/flac-1.3.0/"
+	Write-Output $t1 | Out-File -encoding ASCII .\flaccmd.sh -width 200 -append
+	#$t1="./configure --prefix=`"``echo ~/``Allegro5.1/A5_Deps`""
+	$t1="./configure --prefix=`"/c/mingw`""
+	Write-Output $t1 | Out-File -encoding ASCII .\flaccmd.sh -width 200 -append
+	$t1="make"
+	Write-Output $t1 | Out-File -encoding ASCII .\flaccmd.sh -width 200 -append
+	$t1="make install"
+	Write-Output $t1 | Out-File -encoding ASCII .\flaccmd.sh -width 200 -append
+	$t1="exit"
+	Write-Output $t1 | Out-File -encoding ASCII .\flaccmd.sh -width 200 -append
+	Write-Host "[$NCurrent/$N] `tBuilding the source with Msys.bat --login ..."
+	$DestSh = "C:\MinGW\msys\1.0\etc\profile.d"
+	Write-Host [$NCurrent/$N] Making Directory: .\$DestSh
+	# if folder does not exist...
+	if (!(Test-Path $DestSh)){
+		# create it
+		[void](new-item $DestSh -itemType directory)
+	}
+	#$file = Get-Item "$DestSh"
+	#$file.Attributes = 'Hidden'
+	Move-Item .\flaccmd.sh "$DestSh\" -force
+	#copy $env:HOMEDRIVE$env:HOMEPATH\Allegro5.1\MinGW\msys\1.0\msys.bat .
+	& C:\MinGW\msys\1.0\msys --login #should only need to be called by last ps scripted
 	####################################
-
+}
+if($result2 -eq 1){ 
+	Write-Host "[$NCurrent/$N] Skipping Verified Dependency Packages Installation" 
+}
 	Write-Host
 	$NCurrent += 1
 
 	####################################
 	#    5) DUMB
+	$DUMB = "DUMB"
+	Write-Host "[$NCurrent/$N] Creating DUMB Libary"
+	Write-Host "[$NCurrent/$N] `tMaking Directory: .\$DUMB"
+	$DestDUMB = ".\$DUMB"
+	# if folder does not exist...
+	if (!(Test-Path $DestDUMB)){
+		# create it
+		[void](new-item $DUMB -itemType directory)
+	}
+	cd $DUMB | out-null
+	
+	#$DUMBDL="http://downloads.sourceforge.net/project/dumb/dumb/0.9.3/dumb-0.9.3.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fdumb%2Ffiles%2Fdumb%2F0.9.3%2F&ts=1374430774&use_mirror=iweb"
+	$DUMBDL="http://downloads.sourceforge.net/project/dumb/dumb/0.9.3/dumb-0.9.3.zip?r=http%3A%2F%2Fdumb.sourceforge.net%2Findex.php%3Fpage%3Ddownloads&ts=1374462143&use_mirror=hivelocity"
+	Write-Host "[$NCurrent/$N] `tDownloading: $DUMBDL ..."
+	
+	(New-Object System.Net.WebClient).DownloadFile($DUMBDL,"$env:HOMEDRIVE$env:HOMEPATH\DUMB_src.zip")
+	
+	Move-Item $env:HOMEDRIVE$env:HOMEPATH\DUMB_src.zip .
+	Write-Host "[$NCurrent/$N] `tExtracting the source..."
+	unzip -q DUMB_src.zip
+	del DUMB_src.zip
+	cd dumb-0.9.3
+	Write-Host "[$NCurrent/$N] `tBuilding the source..."
+	Write-Host "[$NCurrent/$N] `tPress M for MinGW, NO for Allegro 4.2..."
+	& C:\MinGW\bin\mingw32-make.exe MINGDIR=C:\MinGW
+	& C:\MinGW\bin\mingw32-make.exe install MINGDIR=C:\MinGW
+	#Write-Host "[$NCurrent/$N] `tCreating MSYS build shell script..."
+	#$t1="#!/bin/sh"
+	#Write-Output $t1 | Out-File -encoding ASCII .\DUMBcmd.sh -width 200
+	#$t1="cd ~/Allegro5.1/A5_Deps/DUMB/dumb-0.9.3/"
+	#Write-Output $t1 | Out-File -encoding ASCII .\DUMBcmd.sh -width 200 -append
+	##$t1="./configure --prefix=`"``echo ~/``Allegro5.1/A5_Deps`""
+	#$t1="./configure --prefix=`"/c/mingw`""
+	#Write-Output $t1 | Out-File -encoding ASCII .\DUMBcmd.sh -width 200 -append
+	#$t1="make"
+	#Write-Output $t1 | Out-File -encoding ASCII .\DUMBcmd.sh -width 200 -append
+	#$t1="make install"
+	#Write-Output $t1 | Out-File -encoding ASCII .\DUMBcmd.sh -width 200 -append
+	#$t1="exit"
+	#Write-Output $t1 | Out-File -encoding ASCII .\DUMBcmd.sh -width 200 -append
+	#Write-Host "[$NCurrent/$N] `tBuilding the source with Msys.bat --login ..."
+	#$DestSh = "C:\MinGW\msys\1.0\etc\profile.d"
+	#Write-Host [$NCurrent/$N] Making Directory: .\$DestSh
+	## if folder does not exist...
+	#if (!(Test-Path $DestSh)){
+	#	# create it
+	#	[void](new-item $DestSh -itemType directory)
+	#}
+	#$file = Get-Item "$DestSh"
+	#$file.Attributes = 'Hidden'
+	#Move-Item .\DUMBcmd.sh "$DestSh\" -force
+	#copy $env:HOMEDRIVE$env:HOMEPATH\Allegro5.1\MinGW\msys\1.0\msys.bat .
+	#& C:\MinGW\msys\1.0\msys --login #should only need to be called by last ps scripted
+
+	
 	####################################
 
 	Write-Host
@@ -367,6 +466,8 @@ if($result -eq 1) {
 
 	####################################
 	#    7) CG Toolkit-bin
+	# Needed for Nvidia GPGPU stuff
+	# https://developer.nvidia.com/cg-toolkit
 	####################################
 
 	Write-Host
@@ -382,6 +483,16 @@ if($result -eq 1) {
 	####################################
 	#    9) FreeType
 	####################################
+	Write-Host
+	$NCurrent += 1
+
+	####################################
+	#    9) PhysFS
+	# http://icculus.org/physfs/downloads/physfs-2.0.3.tar.bz2
+	####################################
+	
+	
+	
 }
 if($result -eq 1) { 
 	Write-Host "[$NCurrent/$N] Skipping Allegro Dependency Installation!" 
